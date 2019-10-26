d68d1ccfd4f776bfcf08f025ca68d7c2f993dae0
cherimo-iwata <chinatsu.29@outlook.jp>
2019-10-27 00:45:18 +0900

    付録問題②

--- JavaTest/src/mondai/Test1.java
+++ JavaTest/src/mondai/Test1.java
@@ -1,10 +1,34 @@
 package mondai;
 
+import java.util.Scanner;
+
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
+
+   System.out.println("年齢を入力してください");
+
+   Scanner scanner = new Scanner(System.in);
+
+   int nenrei = 0;
+   int kakaku = 0;
+
+
+    if (nenrei == 0){
+
+
+	   kakaku =scanner.nextInt(); //年齢が0歳だったら
+
+	   System.out.println("年齢が0の時、価格は "+ kakaku + " 円です");
+
+    }else {
+	   kakaku = scanner.nextInt();  //年齢が0歳でなかったら
+
+	   System.out.println("年齢が0歳ではない時、価格は " + kakaku + " 円です");
+    }
+
+    scanner.close();
 	}
 
 }
