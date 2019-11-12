52569369958ce5bb8bea5681263bb07a4129f3b1
cherimo-iwata <chinatsu.29@outlook.jp>
2019-11-12 22:07:11 +0900

    付録問題8

---
+++ JavaTest/src/mondai/Test8main.java
@@ -0,0 +1,17 @@
+package mondai;
+
+public class Test8main {
+
+	public static void main(String[] args) {
+
+		Test8sub test8sub =new Test8sub();
+
+		int a =0;
+
+		for(int keisan =1; keisan<=3; keisan++) {
+
+		a= test8sub.subA (a);
+	}
+
+}
+}
---
+++ JavaTest/src/mondai/Test8sub.java
@@ -0,0 +1,18 @@
+package mondai;
+
+public class Test8sub {
+
+
+
+	public  int subA (int a) {
+
+
+
+	System.out.println("aの値は"+a+"です");
+	a=a+1;
+
+	return a;
+
+	}
+
+}
